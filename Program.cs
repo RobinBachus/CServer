6557a1c29f25595b45f244382dee7dcf152f2595
RobinBachus <robinbachus@gmail.com>
2022-11-03 00:59:45 +0100

    V0.0.2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace CServer
+﻿using System.Net;
+using static CServer.HttpListenerClass;
+
+
+namespace CServer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HandleHTTPResquest();
         }
+
+        
     }
 }
